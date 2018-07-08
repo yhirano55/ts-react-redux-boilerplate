3c409fb2545c385a3d29683ead361bc652e52f44
Yoshiyuki Hirano <yhirano@me.com>
2018-07-09 07:05:04 +0900

    Configure storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,43 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
   plugins: [
     // your custom plugins
   ],
   module: {
+    // add your custom rules.
     rules: [
-      // add your custom rules.
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]-[local]-[hash:base64:5]",
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                autoprefixer()
+              ]
+            }
+          },
+        ],
+      },
     ],
   },
 };
