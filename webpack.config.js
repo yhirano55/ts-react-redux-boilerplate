eeb7b9fc2662c8bd5e491ec8be0a1fa12b0a9d76
Yoshiyuki Hirano <yhirano@me.com>
2018-07-09 06:46:32 +0900

    Add css-loader to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const isProduction = mode === 'production';
 const outputFilename = isProduction ? 'bundle.[chunkhash].js' : '[name].js';
+const outputCssFilename = isProduction ? 'bundle.[chunkhash].css' : '[name].css';
 
 module.exports = {
   mode: mode,
@@ -19,12 +22,50 @@ module.exports = {
   module: {
     rules: [
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                modules: true,
+                localIdentName: "[name]-[local]-[hash:base64:5]",
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'typed-css-modules-loader',
+              options: {
+                camelCase: true,
+                searchDir: './src',
+                outDir: './typings'
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            }
+          ]
+        })
+      },
     ]
   },
   plugins: [
     new htmlWebpackPlugin({
       template: './src/index.html',
-    })
+    }),
+    new ExtractTextPlugin({
+      filename: outputCssFilename,
+      allChunks: true
+    }),
   ]
 }
